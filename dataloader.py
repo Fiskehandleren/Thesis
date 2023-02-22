541ee1122457ec48e6f7f2b47112b46e2bd3dec5
Fiskehandleren <carljohanastrup@gmail.com>
2023-02-22 14:57:34 +0100

    Dataloader

--- dataloader.py
+++ dataloader.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import datetime 
 import numpy as np
+from scipy import stats
+import os
+import torch
 
 def load_data():
     df = pd.read_csv(
@@ -48,7 +51,7 @@ def create_count_data(df, interval_length=30, save=False):
         ], dtype=object)
         # The the count of 
         res = df[df.Cluster == cluster].explode('Period').groupby(pd.Grouper(key='Period', freq=f'{interval_length}T'))['Id'].nunique()
-        # rename energy to sessions
+        # rename value column to `Sessions``
         res.rename('Sessions', inplace=True)
         data = res.to_frame()
         data = data.reset_index()
@@ -56,8 +59,75 @@ def create_count_data(df, interval_length=30, save=False):
         data["Cluster"] = cluster
         df_combined = pd.concat([df_combined, data])
     df_combined.reset_index(drop=True, inplace=True)
+    df_pivot = df_combined.pivot_table(index='Period', columns='Cluster', values='Sessions')
+    # Cut timeseries off at the latest timepoint of the cluster with the earliest last timepoint
+    df_pivot_reduced = df_pivot.loc[:df_combined.groupby('Cluster').agg({'Period': 'max'}).Period.min()].copy()
+    # Cluster "SHERMAN" has no data before late 2021, so we drop it
+    df_pivot_reduced.drop('SHERMAN', axis=1, inplace=True)
+    # Fill missing values with 0
+    df_pivot_reduced.fillna(0, inplace=True)
     if save:
-        df_combined.to_csv(f'charging_session_count_{interval_length}.csv')
+        df_pivot_reduced.to_csv(f'charging_session_count_{interval_length}.csv')
 
-    return df_combined
+    return df_pivot_reduced
     
+
+def generate_covariates(times, num_covariates=4):
+    """ Generated weekday, hour, and month covariates for each timepoint."""
+    covariates = np.zeros((times.shape[0], num_covariates))
+    for i, input_time in enumerate(times):
+        covariates[i, 1] = input_time.weekday()
+        covariates[i, 2] = input_time.hour
+        covariates[i, 3] = input_time.month
+    # Standardize covariates
+    #for i in range(1,num_covariates):
+     #   covariates[:,i] = stats.zscore(covariates[:,i])
+    return covariates[:, :num_covariates]
+
+
+def generate_dataset(data, seq_len, pred_len, time_len=None, split_ratio=0.8, normalize=False):
+    """
+    :param data: feature matrix
+    :param seq_len: length of the train data sequence
+    :param pred_len: length of the prediction data sequence
+    :param time_len: length of the time series in total
+    :param split_ratio: proportion of the training set
+    :param normalize: scale the data to (0, 1], divide by the maximum value in the data
+    :return: train set (X, Y) and test set (X, Y)
+    """
+    if time_len is None:
+        time_len = data.shape[0]
+    if normalize:
+        max_val = np.max(data)
+        data = data / max_val
+    train_size = int(time_len * split_ratio)
+    train_data = data[:train_size]
+    test_data = data[train_size:time_len]
+    train_X, train_Y, test_X, test_Y = list(), list(), list(), list()
+    for i in range(len(train_data) - seq_len - pred_len):
+        train_X.append(np.array(train_data[i : i + seq_len]))
+        train_Y.append(np.array(train_data[i + seq_len : i + seq_len + pred_len]))
+    for i in range(len(test_data) - seq_len - pred_len):
+        test_X.append(np.array(test_data[i : i + seq_len]))
+        test_Y.append(np.array(test_data[i + seq_len : i + seq_len + pred_len]))
+    return np.array(train_X), np.array(train_Y), np.array(test_X), np.array(test_Y)
+
+
+def generate_torch_datasets(data, seq_len, pred_len, time_len=None, split_ratio=0.8, normalize=False):
+    normalize=False
+    train_X, train_Y, test_X, test_Y = generate_dataset(
+        data,
+        seq_len,
+        pred_len,
+        time_len=time_len,
+        split_ratio=split_ratio,
+        normalize=normalize,
+    )
+    print(train_X)
+    train_dataset = torch.utils.data.TensorDataset(
+        torch.FloatTensor(train_X), torch.FloatTensor(train_Y)
+    )
+    test_dataset = torch.utils.data.TensorDataset(
+        torch.FloatTensor(test_X), torch.FloatTensor(test_Y)
+    )
+    return train_dataset, test_dataset
