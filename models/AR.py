d1c5912f7064940bed433e73b3c03304701edc0e
Fiskehandleren <carljohanastrup@gmail.com>
2023-02-27 13:43:08 +0100

    Fix AR to new dataloader

--- models/AR.py
+++ models/AR.py
@@ -6,20 +6,18 @@ class AR_Net(nn.Module):
     def __init__(self, input_dim, output_dim, hidden_dim):
         super(AR_Net, self).__init__()
         self.fc1 = nn.Linear(input_dim, hidden_dim) 
-        self.fc3 = nn.Linear(hidden_dim, hidden_dim*2) 
-
-        # Non-linearity
         self.relu = nn.ReLU()
-
-        self.fc2 = nn.Linear(hidden_dim*2, output_dim) 
-        # Linear function (readout)
+        self.fc2 = nn.Linear(hidden_dim, output_dim) 
 
     def forward(self, x):
+        # To handle only one feature in the dataloader for AR models
+        x = x.view(x.shape[0], -1)
+
         out = self.fc1(x)
         out = self.relu(out)
-        out = self.fc3(out)
         out = self.fc2(out)
-        return out
+
+        return out.exp()
     
     @staticmethod
     def add_model_specific_arguments(parent_parser):
@@ -35,8 +33,9 @@ class AR(nn.Module):
         print("AR model created with input_dim: ", input_dim, " and output_dim: ", output_dim)
 
     def forward(self, x):
+        x = x.view(x.shape[0], -1)
         out = self.fc1(x)
-        return out
+        return out.exp()
     
     @staticmethod
     def add_model_specific_arguments(parent_parser):
