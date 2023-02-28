2d4de33b7dabff7c10b7ae4f2127b8a7182fefae
Fiskehandleren <carljohanastrup@gmail.com>
2023-02-28 14:09:55 +0100

    Add documentation for censored nll

--- losses.py
+++ losses.py
@@ -7,8 +7,14 @@ def poisson_negative_log_likelihood(y_predict, y):
 
 
 def censored_poisson_negative_log_likelihood(y_predict, y, C):
+    """ 
+    y_predict: lambda for Poisson
+    y: observed data
+    C: censoring threshold
+    https://findit.dtu.dk/en/catalog/53282c10c18e77205dd0f8ae """
+
     pois = torch.distributions.poisson.Poisson(y_predict)
-    """ https://findit.dtu.dk/en/catalog/53282c10c18e77205dd0f8ae """
+    # Pytorch doesn't have the cdf function for the poisson distribution
     poiss_cdf = torch.tensor(poisson.cdf(k=C, mu=y_predict))
     d_t = (y<C).int()
     return -torch.sum(d_t * pois.log_prob(y) + (1 - d_t) * torch.log(poiss_cdf)) # Do we sum on the correct axis here?
