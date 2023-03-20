6dd757e79eb38334373c693b07128789d9889e11
Fiskehandleren <carljohanastrup@gmail.com>
2023-03-20 17:29:34 +0100

    Add Poisson CDF for non-identical k-values

--- utils/losses.py
+++ utils/losses.py
@@ -25,6 +25,26 @@ def poisson_cdf(k, lamb):
         cdf += _pois.log_prob(torch.tensor(i)).exp()
     return cdf
 
+def poisson_cdf_non_identical(k, lamb):
+    """ If k is not a tensor of idential shape to lamb, this will still work.
+        k: tensor of shape (n) !! ALWAYS INTEGER !!
+    """
+    _pois = torch.distributions.Poisson(lamb) # Initialize Poisson distribution with rates
+    cdf = torch.zeros(k.shape) # placeholder
+    n = len(k) # number of samples
+    k_int = torch.ceil(k).int()
+    max_k_int = torch.max(k_int)
+    # Matrix of 0-max_k_int repeated n times across columns, each column being reserved for one value of lambda. 
+    # so each columns is a range of 0-max_k_int.
+    k_mtrx = torch.repeat_interleave(torch.arange(0, max_k_int+1), n).view(max_k_int+1, n)
+    # Calculate the pdf of 0, 1, .., max_k_int for each lambda.
+    pdf_mtrx = _pois.log_prob(k_mtrx).exp()
+    print("torch.max(k_int)", torch.max(k_int))
+    for i in range(len(k)):
+        # Sum the pdf of 0, 1, .., k_int[i] for each lambda to get the cdf for each k
+        cdf[i] = torch.sum(pdf_mtrx[:k_int[i]+1,i])
+    return cdf
+
 def censored_poisson_negative_log_likelihood(y_predict, y, C):
     """ 
     y_predict: lambda for Poisson
