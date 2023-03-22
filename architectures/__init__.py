2f3addcee70652aa3f13c32c8f13a9c03994c0ff
pontius-pilates <80957083+pontius-pilates@users.noreply.github.com>
2023-03-22 14:12:14 +0100

    attempt to add PL module model

--- architectures/__init__.py
+++ architectures/__init__.py
@@ -1,5 +1,6 @@
 from architectures.AR import AR_Net, AR, AR_Net_multi, AR_multi
 from architectures.TGCN import TemporalGCN
 from architectures.LSTM import LSTM
+from architectures.AR_PL import AR_PL, AR_Net_PL
 
-__all__ = ["AR", "AR_Net", "AR_multi", "AR_Net_multi", "TemporalGCN", "LSTM"]
+__all__ = ["AR", "AR_Net", "AR_multi", "AR_Net_multi", "TemporalGCN", "LSTM", "AR_PL", "AR_Net_PL"] 
