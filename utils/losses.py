1679aecf4875e669f790a13307cba6d10be9df9a
Fiskehandleren <carljohanastrup@gmail.com>
2023-03-23 16:15:39 +0100

    Cleanup losses

--- utils/losses.py
+++ utils/losses.py
@@ -13,38 +13,8 @@ def get_loss(loss):
 
 def poisson_negative_log_likelihood(y_predict, y):
     """ https://en.wikipedia.org/wiki/Poisson_regression#Maximum_likelihood-based_parameter_estimation """
-    return -torch.sum(y*y_predict - torch.exp(y_predict))
-
-def poisson_cdf(k, lamb):
-    """Assume k is a tensor where every element is the same!!!!"""
-    _pois = torch.distributions.Poisson(lamb)
-    cdf = torch.zeros(k.shape)
-    k_int = torch.max(k).int()
-    for i in range(k_int+1):
-        cdf += _pois.log_prob(torch.tensor(i)).exp()
-    return cdf
-
-def poisson_cdf_non_identical(k, lamb):
-    """ If k is not a tensor of idential shape to lamb, this will still work.
-        k: tensor of shape (n) !! ALWAYS INTEGER !!
-    """
-    _pois = torch.distributions.Poisson(lamb) # Initialize Poisson distribution with rates
-    cdf = torch.zeros(k.shape) # placeholder
-    n = len(k) # number of samples
-    k_int = torch.ceil(k).int()
-    max_k_int = torch.max(k_int)
-    # Matrix of 0-max_k_int repeated n times across columns, each column being reserved for one value of lambda. 
-    # so each columns is a range of 0-max_k_int.
-    k_mtrx = torch.repeat_interleave(torch.arange(0, max_k_int+1), n).view(max_k_int+1, n)
-    # Calculate the pdf of 0, 1, .., max_k_int for each lambda.
-    pdf_mtrx = _pois.log_prob(k_mtrx).exp()
-    for i in range(n):
-        # Sum the pdf of 0, 1, .., k_int[i] for each lambda to get the cdf for each k
-        cdf[i] = torch.sum(pdf_mtrx[:k_int[i]+1,i])
-    
-    # Sometimes we get a cdf of 1.0000001, which is not allowed. So we set it to 1.
-    return cdf.round(decimals=6)
-
+    pois = torch.distributions.poisson.Poisson(y_predict)
+    return -torch.sum(pois.log_prob(y))
 
 
 def censored_poisson_negative_log_likelihood(y_predict, y, C):
