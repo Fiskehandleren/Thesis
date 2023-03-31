aa3f3d98c505ffacd749dab8de607f95e53bf52e
Fiskehandleren <carljohanastrup@gmail.com>
2023-03-31 16:14:02 +0100

    Align arg-names in models, fix LSTM and GRU input dimensions, and try to add support for using multiple layers (not sure it's correct)

--- tasks/GRU_task.py
+++ tasks/GRU_task.py
@@ -23,25 +23,29 @@ class GRU_Task(pl.LightningModule):
         self.num_layers = num_layers
         self.censored = censored
         self._loss_fn = loss_fn
+        self.learning_rate = learning_rate
+        self.weight_decay = weight_decay
         self.feat_max_val = feat_max_val
         
-        self.lstm = nn.GRU(
+        self.gru = nn.GRU(
             input_size=input_dim,
             hidden_size=hidden_units,
             batch_first=True,
             num_layers=self.num_layers
         )
 
-        self.linear = nn.Linear(in_features=hidden_units, out_features=1)
+        self.linear = nn.Linear(in_features=hidden_units, out_features=output_dim)
         
 
     def forward(self, x):
         batch_size = x.shape[0]
-        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
-        c0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
+        # h0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
+        # c0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
         
-        _, (hn, _) = self.lstm(x, (h0, c0))
-        out = self.linear(hn[0]).flatten()  # First dim of Hn is num_layers, which is set to 1 above.
+        #_, (hn, _) = self.gru(x, (h0, c0))
+        x = x.view(batch_size, -1)
+        _, (hn, _) = self.gru(x)
+        out = self.linear(hn[-1]).flatten()  # First dim of Hn is num_layers, which is set to 1 above.
 
         return out.exp()
     
@@ -64,15 +68,14 @@ class GRU_Task(pl.LightningModule):
         return loss
 
     def configure_optimizers(self):
-        optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = optim.Adam(self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay)
         return optimizer
-    
-    
     
     @staticmethod
     def add_model_specific_arguments(parent_parser):
         parser = argparse.ArgumentParser(parents=[parent_parser], add_help=False)
-        parser.add_argument("--hidden_units", type=int, default=72)
+        parser.add_argument("--hidden_dim", type=int, default=72)
         parser.add_argument("--num_layers", type=int, default=1)
+        parser.add_argument("--output_dim", type=int, default=1)
         return parser
 
--- tasks/LSTM_task.py
+++ tasks/LSTM_task.py
@@ -12,7 +12,7 @@ class LSTM_Task(pl.LightningModule):
         output_dim, 
         loss_fn,
         censored,
-        hidden_units,
+        hidden_dim,
         num_layers: int = 1,
         learning_rate: float = 1e-3,
         weight_decay: float = 1.5e-3,
@@ -21,7 +21,7 @@ class LSTM_Task(pl.LightningModule):
     ):
         super().__init__()
         self.save_hyperparameters()
-        self.hidden_units = hidden_units
+        self.hidden_dim = hidden_dim
         self.num_layers = num_layers
         self.censored = censored
         self._loss_fn = loss_fn
@@ -29,28 +29,36 @@ class LSTM_Task(pl.LightningModule):
         
         self.lstm = nn.LSTM(
             input_size=input_dim,
-            hidden_size=self.hidden_units,
+            hidden_size=self.hidden_dim,
             batch_first=True,
             num_layers=self.num_layers
         )
 
-        self.linear = nn.Linear(in_features=self.hidden_units, out_features=output_dim)
+        self.linear = nn.Linear(in_features=self.hidden_dim, out_features=output_dim)
         
         #self.activation = nn.Sigmoid()
 
     def forward(self, x):
         batch_size = x.shape[0]
-        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
-        c0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
+        # If no h0 and c0 is given to the model, they're initialized to zeros
+        # These two caused issues so outcommented. Why am I writing in English?
+
+        # h0 = torch.zeros(self.num_layers, batch_size, self.hidden_dim).requires_grad_()
+        # c0 = torch.zeros(self.num_layers, batch_size, self.hidden_dim).requires_grad_()
         
-        _, (hn, _) = self.lstm(x, (h0, c0))
-        out = self.linear(hn[0]).flatten()  # First dim of Hn is num_layers, which is set to 1 above.
+        x = x.view(batch_size, -1)
+        _, (hn, _) = self.lstm(x)
+
+        #:math:`(\text{num\_layers}, N, H_{out})` containing the
+        # final hidden state for each element in the sequence.
+        out = self.linear(hn[-1]).flatten()  # First dim of Hn is num_layers, which is set to 1 above.
+        # Should we always grab the last layer? [-1] indicese last eleement
 
         return out.exp()
     
     def training_step(self, batch, batch_idx):
         # Run forward calculation
-        if self.censored == True:
+        if self.censored:
             x, y, tau = batch
         else:
             x, y = batch
@@ -58,7 +66,7 @@ class LSTM_Task(pl.LightningModule):
         y_predict = self(x).view(-1)
 
         # Compute loss.
-        if self.censored == True:
+        if self.censored:
             loss = self._loss_fn(y_predict, y, tau)
         else:
             loss = self._loss_fn(y_predict, y)
@@ -70,13 +78,11 @@ class LSTM_Task(pl.LightningModule):
         optimizer = optim.Adam(self.parameters(), lr=1e-3)
         return optimizer
     
-    
-    
     @staticmethod
     def add_model_specific_arguments(parent_parser):
         parser = argparse.ArgumentParser(parents=[parent_parser], add_help=False)
-        #parser.add_argument("--hidden_units", type=int, default=72)
-        #parser.add_argument("--num_layers", type=int, default=1)
+        parser.add_argument("--hidden_dim", type=int, default=72)
+        parser.add_argument("--num_layers", type=int, default=1)
         #parser.add_argument("--output_dim", type=int, default=1)
         return parser
 
