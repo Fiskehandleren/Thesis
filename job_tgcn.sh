ae0bb2e83c9b3b701f6b4777fa4e54396fdfd94c
fiskehandleren <carlisafk@live.dk>
2023-04-05 11:16:32 +0200

    HPC stuff

--- job_tgcn.sh
+++ job_tgcn.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 ### General options
 ### - specify queue --
+### classstat 
+### check if gpu is available on queue with `nodestat -g <queue_name>`
 #BSUB -q gpuv100
 ### -- set the job Name --
-#BSUB -J tgcn_experiment
+#BSUB -J tgcn
 ### -- ask for number of cores (default: 1) --
 #BSUB -n 1
 ### -- Select the resources: 1 gpu in exclusive process mode --
 #BSUB -gpu "num=1:mode=exclusive_process"
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
-#BSUB -W 24:00
+#BSUB -W 04:00
 # request 5GB of system-memory
 #BSUB -R "rusage[mem=5GB]"
 ### -- set the email address --
@@ -25,5 +27,6 @@
 #BSUB -oo job_out/gpu-%J.out
 #BSUB -eo job_out/gpu_%J.err
 # -- end of LSF options --
+bash python3 -m wandb agent fiskehandleren/Thesis/h14qkbo0
 
-bash python main.py 
+## bash python3 main.py ....
--- setup_hpc.sh
+++ setup_hpc.sh
@@ -1,22 +1,18 @@
-#Open interactive node - Login to login2.hpc.dtu.dk & login3.hpc.dtu.dk
-
-#ssh sXXXXXX@login3.hpc.dtu.dk
-#Enter Password
+#!/bin/bash
 
 #Open interactive GPU node
-voltash
+# voltash
 
-#Load preinstalled modules
 module load cuda/11.6
+#module load python3/3.10.7
+module load python3/3.8.2
 
-# if no conda, follow this https://www.hpc.dtu.dk/?page_id=3678 
+#python3 -m pip install --user --upgrade pip
 
-source $HOME/miniconda3/bin/activate
-conda install python=
+python3 -m venv thesis
+source thesis/bin/activate
+
 pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116
 pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-1.13.0+cu116.html
 pip install torch-geometric-temporal
-
 pip install -r requirements.txt
-
-echo "Done"
