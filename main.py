1760313d44f013d7d522c2a1564cefa0c6f45f6f
fiskehandleren <carlisafk@live.dk>
2023-04-12 14:23:31 +0200

    Fix missing comma

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ if __name__ == "__main__":
     df_dates = pd.DataFrame(dm.y_dates, columns=['Date'])
     df_true = pd.DataFrame(model.test_y, columns=dm.cluster_names)
     df_pred = pd.DataFrame(model.test_y_hat, columns=np.char.add(dm.cluster_names, '_pred'))
-    df_uncensored = pd.DataFrame(model.test_y_true columns=np.char.add(dm.cluster_names, '_true'))
+    df_uncensored = pd.DataFrame(model.test_y_true, columns=np.char.add(dm.cluster_names, '_true'))
 
     preds = pd.concat([df_dates, df_true, df_pred, df_uncensored], axis=1)
     preds.to_csv(f"predictions/predictions_{args.model_name}_{args.loss}_{args.hidden_dim}_{args.censor_level}.csv")
