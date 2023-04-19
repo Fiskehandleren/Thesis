aa316b6e345cc00ec37a822eea21d84cb78eb48c
pontius-pilates <jusi@adm.dtu.dk>
2023-04-19 09:12:13 +0200

    spelling

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -249,7 +249,7 @@ def get_datasets_NN(target, forecast_lead, add_month=True, add_hour=True, add_da
 
         df_test = df_test[features]
 
-        ## Remove tau so it isnt and input feature
+        ## Remove tau so it isnt an input feature
         features.remove(target + '_TAU')
         features.remove(target + '_TRUE')
 
