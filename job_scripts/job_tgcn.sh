fbd57be3077bda7133c321cd955a221a57da43b4
Fiskehandleren <carljohanastrup@gmail.com>
2023-05-03 20:53:06 +0200

    Job #1 tgcn pnll

--- job_scripts/job_tgcn.sh
+++ job_scripts/job_tgcn.sh
@@ -11,7 +11,7 @@
 ### -- Select the resources: 1 gpu in exclusive process mode --
 #BSUB -gpu "num=1:mode=exclusive_process"
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
-#BSUB -W 24:00
+#BSUB -W 12:00
 # request 10GB of system-memory
 #BSUB -R "rusage[mem=10GB]"
 ### -- set the email address --
@@ -24,7 +24,8 @@
 #BSUB -N
 ### -- Specify the output and error file. %J is the job-id --
 ### -- -o and -e mean append, -oo and -eo mean overwrite --
-#BSUB -oo job_out/gpu_%J.out
+## BSUB -oo job_out/gpu_%J.out
 #BSUB -eo job_out/gpu_%J.err
 # -- end of LSF options --
-bash python3 main.py --loss=CPNLL --mode=train --logger --devices=1 --val_end=2019-05-02 --censored --test_end=2019-06-30 --max_steps=-1 --num_nodes=1 --precision=32 --train_end=2019-01-01 --batch_size=67 --dataloader=EVChargersDatasetSpatial --hidden_dim=38 --max_epochs=10 --model_name=TGCN --accelerator=gpu --train_start=2017-01-01 --censor_level=2 --forecast_lead=1 --learning_rate=0.0001 --no_self_loops --censor_dynamic --inference_mode --sequence_length=336 --track_grad_norm=-1 --forecast_horizon=1 --log_every_n_steps=50 --enable_progress_bar --replace_sampler_ddp --enable_checkpointing --enable_model_summary --num_sanity_val_steps=2 --check_val_every_n_epoch=1 --multiple_trainloader_mode=max_size_cycle
+# bash python3 main.py --loss=CPNLL --mode=train --logger --devices=1 --val_end=2019-05-02 --censored --test_end=2019-06-30 --max_steps=-1 --num_nodes=1 --precision=32 --train_end=2019-01-01 --batch_size=67 --dataloader=EVChargersDatasetSpatial --hidden_dim=38 --max_epochs=10 --model_name=TGCN --accelerator=gpu --train_start=2017-01-01 --censor_level=2 --forecast_lead=1 --learning_rate=0.0001 --no_self_loops --censor_dynamic --inference_mode --sequence_length=336 --track_grad_norm=-1 --forecast_horizon=1 --log_every_n_steps=50 --enable_progress_bar --replace_sampler_ddp --enable_checkpointing --enable_model_summary --num_sanity_val_steps=2 --check_val_every_n_epoch=1 --multiple_trainloader_mode=max_size_cycle
+bash python3 -m wandb agent latend-demand/Thesis/65kr4ys3
