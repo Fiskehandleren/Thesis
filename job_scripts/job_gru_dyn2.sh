f8f556aa0f0c6c1a03e97090f07f4c2cbc16ad2c
pontius-pilates <jusi@adm.dtu.dk>
2023-05-16 10:40:45 +0200

    slight changes to jobs

--- job_scripts/job_gru_dyn2.sh
+++ job_scripts/job_gru_dyn2.sh
@@ -5,7 +5,7 @@
 ### check if gpu is available on queue with `nodestat -g <queue_name>`
 #BSUB -q gpuv100
 ### -- set the job Name --
-#BSUB -J gru_cpnll_all_clusters
+#BSUB -J gru_cpnll
 ### -- ask for number of cores (default: 1) --
 #BSUB -n 8
 ### -- Select the resources: 1 gpu in exclusive process mode --
@@ -28,19 +28,9 @@
 #BSUB -eo job_out/gpu_%J.err
 # -- end of LSF options --
 
-## CPNLL
-bash python3 -m wandb agent latend-demand/Thesis/dtb5ld7a
-
-## bash python3 main.py --model_name GRU --cluster WEBSTER --train_start 2018-01-01 --train_end 2019-01-01 --val_end 2019-05-02 --test_end 2019-06-30 --sequence_length 303 --hidden_dim 97 --batch_size 24 --max_epochs 10 --save_predictions --learning_rate 0.0005 --weight_decay 0.02526 --dataloader EVChargersDataset  --loss CPNLL --censor_level 2 --censor_dynamic --censored --accelerator gpu --devices 1
-
-
-## PNLL
-## bash python3 main.py --model_name GRU --cluster WEBSTER --train_start 2018-01-01 --train_end 2019-01-01 --val_end 2019-06-30 --test_end 2019-05-02 --sequence_length 303 --hidden_dim 97 --batch_size 17 --max_epochs 15 --learning_rate 0.01931 --weight_decay 0.02526 --dataloader EVChargersDataset --loss PNLL --censor_level 2 --censor_dynamic --accelerator gpu --devices 1
-##bash python3 -m wandb agent latend-demand/Thesis/dy1cerq0
-
-
 CLUSTERS=("BRYANT" "MPL" "CAMBRIDGE" "RINCONADA" "HAMILTON" "TED" "HIGH" "WEBSTER")
 n=${#CLUSTERS[@]}
+
 for i in $(seq 0 "$(($n-1))")
   do
   bash python3 main.py --loss=CPNLL --mode=train --logger --cluster "${CLUSTERS[$i]}"  --devices=1 --val_end=2019-05-02 \
