a25baeb2f0fdbe9376f66d50d458eb1a0d8b8ab8
Fiskehandleren <carljohanastrup@gmail.com>
2023-05-31 12:26:15 +0200

    boi

--- job_scripts/job_atgcn.sh
+++ job_scripts/job_atgcn.sh
@@ -5,7 +5,7 @@
 ### check if gpu is available on queue with `nodestat -g <queue_name>`
 #BSUB -q gpuv100
 ### -- set the job Name --
-#BSUB -J atgcn_with_best_params_336
+#BSUB -J tgcn_sweep
 ### -- ask for number of cores (default: 1) --
 #BSUB -n 8
 ### -- Select the resources: 1 gpu in exclusive process mode --
@@ -24,9 +24,9 @@
 #BSUB -N
 ### -- Specify the output and error file. %J is the job-id --
 ### -- -o and -e mean append, -oo and -eo mean overwrite --
-#BSUB -oo job_out/gpu_%J.out
+#BSUB -o /dev/null
 #BSUB -eo job_out/gpu_%J.err
 # -- end of LSF options --
-## bash python3 -m wandb agent latend-demand/Thesis/j885stwf
+bash python3 -m wandb agent latend-demand/Thesis/mwr3zzii
 
-bash python3 main.py --loss=CPNLL --mode=train --logger --devices=1 --val_end=2019-05-02 --censored --test_end=2019-06-30 --max_steps=-1 --num_nodes=1 --precision=32 --train_end=2019-01-01 --batch_size=67 --dataloader=EVChargersDatasetSpatial --hidden_dim=38 --max_epochs=10 --model_name=ATGCN --accelerator=gpu --train_start=2017-01-01 --censor_level=2 --weight_decay=0.0998543610822035 --forecast_lead=1 --learning_rate=0.04273100373245771 --no_self_loops --censor_dynamic --inference_mode --sequence_length=336 --track_grad_norm=-1 --forecast_horizon=1 --log_every_n_steps=50 --enable_progress_bar --replace_sampler_ddp --enable_checkpointing --enable_model_summary --num_sanity_val_steps=2 --check_val_every_n_epoch=1 --multiple_trainloader_mode=max_size_cycle
+# bash python3 main.py --loss=CPNLL --mode=train --logger --devices=1 --val_end=2019-05-02 --censored --test_end=2019-06-30 --max_steps=-1 --num_nodes=1 --precision=32 --train_end=2019-01-01 --batch_size=67 --dataloader=EVChargersDatasetSpatial --hidden_dim=38 --max_epochs=10 --model_name=ATGCN --accelerator=gpu --train_start=2017-01-01 --censor_level=2 --weight_decay=0.0998543610822035 --forecast_lead=1 --learning_rate=0.04273100373245771 --no_self_loops --censor_dynamic --inference_mode --sequence_length=336 --track_grad_norm=-1 --forecast_horizon=1 --log_every_n_steps=50 --enable_progress_bar --replace_sampler_ddp --enable_checkpointing --enable_model_summary --num_sanity_val_steps=2 --check_val_every_n_epoch=1 --multiple_trainloader_mode=max_size_cycle
