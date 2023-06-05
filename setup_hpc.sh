6e350d139ef78cc0f9add89c46e985f913f1d0cd
pontius-pilates <80957083+pontius-pilates@users.noreply.github.com>
2023-06-05 13:51:32 +0200

    update queue

--- setup_hpc.sh
+++ setup_hpc.sh
@@ -13,7 +13,8 @@ source thesis/bin/activate
 # export PYTHONPATH='
 pip install --upgrade pip
 
-pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116
+#pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116
+pip install torch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 
 pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-1.13.0+cu116.html
 pip install torch-geometric-temporal
 pip install -r requirements.txt
