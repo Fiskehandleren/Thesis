5d3a460b4f51a7bfd99aa45031b8c774dc2f55cc
pontius-pilates <80957083+pontius-pilates@users.noreply.github.com>
2023-06-14 11:47:04 +0200

    updated unaware losses (missing 3 for gru and lstm in total)

--- job_scripts/job_gru_dyn1.sh
+++ job_scripts/job_gru_dyn1.sh
@@ -39,7 +39,7 @@ for j in 1 2 3 4
     bash python3 main.py --loss=PNLL --mode=train --logger --cluster "${CLUSTERS[$i]}"  --devices=1  \
     --train_start=2017-01-01 --train_end=2018-10-01 --val_end=2019-04-01 --test_end=2019-07-01 --max_steps=-1 --num_nodes=1 --precision=32 \
     --covariates --batch_size=51 --dataloader=EVChargersDataset --hidden_dim=238 --max_epochs=20 \
-    --model_name=GRU --num_layers=1 --accelerator=gpu  --censor_level=1 --weight_decay=0.00268538443015154 \
+    --model_name=GRU --num_layers=1 --accelerator=gpu  --censor_level=2 --weight_decay=0.00268538443015154 \
     --forecast_lead=48 --learning_rate=0.00070386 --censor_dynamic --inference_mode --sequence_length=336 --track_grad_norm=-1 \
     --forecast_horizon=1 --log_every_n_steps=50 --enable_progress_bar --replace_sampler_ddp --enable_checkpointing \
     --enable_model_summary --num_sanity_val_steps=2 --check_val_every_n_epoch=1 --multiple_trainloader_mode=max_size_cycle
